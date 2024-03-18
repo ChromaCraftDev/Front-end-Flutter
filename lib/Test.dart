c8fbff1523ffc4660cacdf7d61b18fdb045a21d0
sadewasadewal <130735842+sadewasadewal@users.noreply.github.com>
2024-03-18 20:55:59 +0530

    did the AI page in Test.dart

--- lib/Test.dart
+++ lib/Test.dart
@@ -1,117 +1,216 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(MyApp());
+  runApp(ChromaCraftApp());
 }
 
-class MyApp extends StatelessWidget {
+class ChromaCraftApp extends StatelessWidget {
+  // Define the colors for the palettes
+  final List<Color> paletteColors = [
+    Colors.red, // Base
+    Colors.green, // Primary
+    Colors.blue, // Shade
+    Colors.yellow, // Alternate
+    Colors.orange, // Container
+    Colors.purple, // Error
+    Colors.brown, // Text
+    Colors.teal, // Warning
+    Colors.grey, // Subtle
+    Colors.pink, // Success
+  ];
+
+  // Define corresponding labels for each color
+  final List<String> colorLabels = [
+    'Base',
+    'Primary',
+    'Shade',
+    'Alternate',
+    'Container',
+    'Error',
+    'Text',
+    'Warning',
+    'Subtle',
+    'Success',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Text Generation with Google AI',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: TextGenerationScreen(),
-    );
-  }
-}
-
-class TextGenerationScreen extends StatefulWidget {
-  @override
-  _TextGenerationScreenState createState() => _TextGenerationScreenState();
-}
-
-class _TextGenerationScreenState extends State<TextGenerationScreen> {
-  TextEditingController _promptController = TextEditingController();
-  String _generatedText = '';
-  bool _loading = false;
-
-  Future<void> _generateText(String prompt) async {
-    setState(() {
-      _loading = true;
-    });
-
-    const String apiKey = 'AIzaSyBXe8a9jS50NmXW6T_FpDjdubZ1nd-RPvM';
-    const String endpoint ='https://language.googleapis.com/v1/documents:analyzeText';
-
-    final Map<String, dynamic> requestBody = {
-      'document': {
-        'type': 'PLAIN_TEXT',
-        'content': prompt,
-      },
-      'features': {
-        'extractSyntax': false,
-        'extractEntities': false,
-        'extractDocumentSentiment': false,
-        'extractEntitySentiment': false,
-        'classifyText': true,
-      },
-    };
-
-    final Uri uri = Uri.parse('$endpoint?key=$apiKey');
-
-    final http.Response response =
-        await http.post(uri, body: jsonEncode(requestBody));
-
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> data = jsonDecode(response.body);
-      final String generatedText = data['text'] as String;
-      setState(() {
-        _generatedText = generatedText;
-        _loading = false;
-      });
-    } else {
-      setState(() {
-        _generatedText = 'Failed to generate text';
-        _loading = false;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Text Generation with Google AI'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextField(
-              controller: _promptController,
-              decoration: const InputDecoration(
-                labelText: 'Enter Prompt',
+      title: 'ChromaCraft',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Generate Palette'),
+        ),
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              DrawerHeader(
+                decoration: const BoxDecoration(
+                    color: Color.fromARGB(200, 79, 55, 140)),
+                padding: const EdgeInsets.all(40.0),
+                child: Image.asset(
+                  'Images/logo2.PNG',
+                  width: 1000, // Adjust width as needed
+                  height: 1000, // Adjust height as needed
+                ),
+              ),
+              Expanded(
+                child: ListView(
+                  padding: EdgeInsets.zero,
+                  children: <Widget>[
+                    ListTile(
+                      leading: const Icon(Icons.settings),
+                      title: const Text('Configure'),
+                      onTap: () {
+                        Navigator.pushNamed(context, '/config');
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.web),
+                      title: const Text('Browse Template'),
+                      onTap: () {
+                        Navigator.pushNamed(context, '/browse');
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.create),
+                      title: const Text('Generate Template'),
+                      onTap: () {
+                        Navigator.pushNamed(context, '/ai');
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.settings),
+                      title: const Text('Settings'),
+                      onTap: () {
+                        Navigator.pushNamed(context, '/settings');
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.account_circle), // Icon for Profile
+                title: const Text('Profile'),
+                onTap: () {
+                  Navigator.pushNamed(context, '/profile');
+                },
+              ),
+            ],
+          ),
+        ),
+        body: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  // Image Prompt section
+                  Container(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.network(
+                        'https://images.unsplash.com/photo-1579546929518-9e396f3cc809?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D'),
+                  ),
+                  // Text Prompt section
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              labelText: 'Text prompt',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Add functionality for the generate button
+                          },
+                          child: Text('Generate'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _loading
-                  ? null
-                  : () {
-                      final String prompt = _promptController.text.trim();
-                      if (prompt.isNotEmpty) {
-                        _generateText(prompt);
-                      }
-                    },
-              child: _loading
-                  ? const CircularProgressIndicator()
-                  : const Text('Generate Text'),
-            ),
-            const SizedBox(height: 20),
+            // Color palettes
             Expanded(
-              child: SingleChildScrollView(
-                child: Text(
-                  _generatedText,
-                  style: const TextStyle(fontSize: 16),
-                ),
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: paletteColors.length ~/ 2,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          margin: const EdgeInsets.all(4.0),
+                          decoration: BoxDecoration(
+                            color: paletteColors[index],
+                            borderRadius:
+                                BorderRadius.circular(8.0), // Add border radius
+                          ),
+                          height: 80, // Adjust height as needed
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                colorLabels[index],
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: paletteColors.length ~/ 2,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          margin: const EdgeInsets.all(4.0),
+                          decoration: BoxDecoration(
+                            color: paletteColors[index + paletteColors.length ~/ 2],
+                            borderRadius:
+                                BorderRadius.circular(8.0), // Add border radius
+                          ),
+                          height: 80, // Adjust height as needed
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                colorLabels[index + paletteColors.length ~/ 2],
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
+        ),
+        // Apply button
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Align(
+            alignment: Alignment.topRight,
+            child: FloatingActionButton(
+              onPressed: () {},
+              child: const Icon(Icons.check),
+            ),
+          ),
         ),
       ),
     );

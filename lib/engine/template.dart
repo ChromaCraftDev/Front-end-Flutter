31711be0267707628b44fe21ee5957551cd5a35e
aemogie <54412618+aemogie@users.noreply.github.com>
2024-03-22 15:14:22 +0530

    fix: pull before pushing, always

--- lib/engine/template.dart
+++ lib/engine/template.dart
@@ -3,9 +3,15 @@ import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'config.dart';
 
 String template(Config config, String input) {
-  var curr = input;
-  for (final ColorOption(name: name, color: color) in config.semanticColors) {
-    curr.replaceAll(name, color.toString());
-  }
-  return curr;
+  return input.replaceAllMapped(
+    RegExp(r"\{\{\s*[c]\.(\w+)\s*(\|.*)?\}\}", caseSensitive: false),
+    (match) {
+      final option = config.semanticColors
+          .where((it) => it.name == match.group(1)!)
+          .firstOrNull;
+      return option != null
+          ? colorToHex(option.color, toUpperCase: false, enableAlpha: false)
+          : "invalid";
+    },
+  );
 }

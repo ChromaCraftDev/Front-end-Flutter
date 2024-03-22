458fc62a5295b225eda3fd93bfe42283151f819f
Wizardatgit <159108003+Wizardatgit@users.noreply.github.com>
2024-03-22 19:22:44 +0530

    feat: rainbow colors

--- lib/engine/config.dart
+++ lib/engine/config.dart
@@ -1,15 +1,15 @@
-import 'dart:io';
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/rendering.dart';
 
 class ColorOption {
   final Color original;
   Color color;
   final String name;
-  final String description;
+  final String? description;
 
-  ColorOption(this.original, this.name, this.description) : color = original;
+  ColorOption(this.original, this.name, {this.description}) : color = original;
 }
 
 class FontOption {
@@ -20,57 +20,73 @@ class FontOption {
 
 final config = Config();
 
+extension _OptionMap on List<ColorOption> {
+  Map<String, ColorOption> _optionMap() => {for (final it in this) it.name: it};
+}
+
 class Config {
-  final List<ColorOption> semanticColors = [
+  final semanticColors = [
     ColorOption(
       const Color(0xFF1E1E2E),
       'base',
-      'The most prevalent.\nUsually the background colour.',
+      description: 'The most prevalent.\nUsually the background colour.',
     ),
     ColorOption(
       const Color(0xFF11111B),
       'shade',
-      'Visually below the components using base colour.\nUsually used for sidebars',
+      description:
+          'Visually below the components using base colour.\nUsually used for sidebars',
     ),
     ColorOption(
       const Color(0xFF313244),
       'container',
-      'Visually above the components using base.\nUsually used for cards.',
+      description:
+          'Visually above the components using base.\nUsually used for cards.',
     ),
     ColorOption(
       const Color(0xFFCDD6F4),
       'text',
-      'Forground.\nUsually used for main text, and icons.',
+      description: 'Forground.\nUsually used for main text, and icons.',
     ),
     ColorOption(
       const Color(0xFFA6ADC8),
       'subtle',
-      'Less important than "text".\nUsally used for description texts',
+      description:
+          'Less important than "text".\nUsally used for description texts',
     ),
     ColorOption(
       const Color(0xFFCBA6F7),
       'primary',
-      'The focus.\nUsually used for important buttons.',
+      description: 'The focus.\nUsually used for important buttons.',
     ),
     ColorOption(
       const Color(0xFFF5C2E7),
       'alternate',
-      'A contracting accent used to create visual interest.',
+      description: 'A contracting accent used to create visual interest.',
     ),
     ColorOption(
       const Color(0xFFF38BA8),
       'error',
-      'Used to indicate Error',
+      description: 'Used to indicate Error',
     ),
     ColorOption(
       const Color(0xFFFAB387),
       'warning',
-      'Used to indicate Warning',
+      description: 'Used to indicate Warning',
     ),
     ColorOption(
       const Color(0xFFA6E3A1),
       'success',
-      'Used to indicate success.',
+      description: 'Used to indicate success.',
     ),
+  ];
+  final rainbowColors = [
+    ColorOption(const Color(0xFFF38BA8), 'red'),
+    ColorOption(const Color(0xFFFAB387), 'orange'),
+    ColorOption(const Color(0xFFF9E2AF), 'yellow'),
+    ColorOption(const Color(0xFFA6E3A1), 'green'),
+    ColorOption(const Color(0xFF94E2D5), 'cyan'),
+    ColorOption(const Color(0xFF89B4FA), 'blue'),
+    ColorOption(const Color(0xFFCBA6F7), 'purple'),
   ];
 }

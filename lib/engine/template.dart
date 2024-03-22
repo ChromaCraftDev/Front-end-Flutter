131602c1b12e837b69b6a9d2e77de78ecba6de5a
aemogie <54412618+aemogie@users.noreply.github.com>
2024-03-22 20:26:14 +0530

    feat: function calls in templates

--- lib/engine/template.dart
+++ lib/engine/template.dart
@@ -1,17 +1,40 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 import 'config.dart';
 
+final Map<String, Color Function(Color, Iterable<String>)> operations = {
+  "smartLighten": (it, args) {
+    final t = double.parse(args.first);
+    final hsv = HSVColor.fromColor(it);
+    if (ThemeData.estimateBrightnessForColor(it) == Brightness.dark) {
+      return hsv.withValue(hsv.value + t).toColor();
+    } else {
+      return hsv.withValue(hsv.value - t).toColor();
+    }
+  },
+};
+
 String template(Config config, String input) {
   return input.replaceAllMapped(
-    RegExp(r"\{\{\s*[c]\.(\w+)\s*(\|.*)?\}\}", caseSensitive: false),
+    RegExp(r"\{\{\s*[c]\.(\w+)\s*(?:\|\s*(.*)\s*)?\}\}", caseSensitive: false),
     (match) {
       final option = (config.semanticColors + config.rainbowColors)
           .where((it) => it.name == match.group(1)!)
           .firstOrNull;
-      return option != null
-          ? colorToHex(option.color, toUpperCase: false, enableAlpha: false)
-          : "invalid";
+      if (option == null) return "invalid";
+      final ops = match
+          .groups(List.generate(match.groupCount - 2, (i) => i + 2))
+          .map((it) {
+        final tokens = it!.split(RegExp("\\s+"));
+        print("'${tokens[0]}'");
+        return (Color it) => operations[tokens[0]]!.call(it, tokens.skip(1));
+      });
+      var modified = Color(option.color.value);
+      for (final op in ops) {
+        modified = op(modified);
+      }
+      return colorToHex(modified, toUpperCase: false, enableAlpha: false);
     },
   );
 }
